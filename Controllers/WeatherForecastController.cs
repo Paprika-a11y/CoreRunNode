1d7daa0f10da9de6594eadf5a1ce832ed770e301
doris <doris@hanxia.onmicrosoft.com>
2020-12-22 17:44:01 +0800

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace RunNodeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet]
+        [Route("/test")]
+        public string test() {
+
+            ProcessStartInfo startinfo = new ProcessStartInfo();
+            startinfo.FileName = "bash";
+            startinfo.FileName = "/etc/opt/nodejs/14.15.0/bin/node";
+            Process process = new Process();
+            process.StartInfo = startinfo;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            process.StandardInput.WriteLine("node -v");
+            string line = string.Empty;
+            
+            while (!process.StandardOutput.EndOfStream)
+            {
+                line = process.StandardOutput.ReadLine();
+
+                _logger.LogInformation(line);
+            }
+            process.WaitForExit();
+            return string.Empty;
+            
+        }
+
+    }
+}
